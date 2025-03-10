bed70a3a2c8e885cd06d52e169382820ebdcc9d2
scxorps <mohamed79163@gmail.com>
2025-03-10 19:05:39 +0100

    added usermanager

--- core/models.py
+++ core/models.py
@@ -3,13 +3,40 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import BaseUserManager
 import re
 
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        phone_number = extra_fields.get("phone_number")
+        if not phone_number:
+            raise ValueError("Superuser must have a phone number.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
 class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False,max_length=255)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    
+    objects = UserManager()
+    
+    REQUIRED_FIELDS  = ['email','phone_number']
     
     def clean(self):
         pattern=r"^\+\d{1,3}[-\s]?\d{9}$"
@@ -19,10 +46,11 @@ class User(AbstractUser):
             raise ValidationError("Phone number already exists")
         
     def save(self, *args, **kwargs):
-        # If the password is not already hashed, hash it
         self.clean()
-        if self.pk is None or not self.password.startswith('pbkdf2_'):
+        
+        if not self.password.startswith('pbkdf2_') and self.pk is None:
             self.set_password(self.password)
+
         super().save(*args, **kwargs)
 
 
